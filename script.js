5122bd77df454c5d76e958cbae589a547a2ef26f
nethrasaravanan <nethrasaravanan2004@gmail.com>
2026-08-05 18:31:30 +0530

    Added EmailJS Contact Form

--- script.js
+++ script.js
@@ -100,3 +100,34 @@ topBtn.onclick = function(){
     });
 
 }
+// ==========================
+// EmailJS Contact Form
+// ==========================
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    emailjs.sendForm(
+        "service_zens6n8",
+        "template_mncrpw6",
+        this
+    )
+    .then(function () {
+
+        alert("✅ Message sent successfully!");
+
+        form.reset();
+
+    })
+    .catch(function (error) {
+
+        alert("❌ Failed to send message.");
+
+        console.log(error);
+
+    });
+
+});
