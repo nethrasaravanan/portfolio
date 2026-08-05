90b95295326af0948bdf31a053e25db382d8bbd6
nethrasaravanan <nethrasaravanan2004@gmail.com>
2026-08-05 17:55:28 +0530

    Added resume

--- script.js
+++ script.js
@@ -37,3 +37,66 @@ function typeEffect() {
 }
 
 typeEffect();
+const themeBtn = document.getElementById("theme-toggle");
+
+themeBtn.onclick = function () {
+
+    document.body.classList.toggle("light-mode");
+
+    if(document.body.classList.contains("light-mode")){
+        themeBtn.innerHTML="🌙";
+    }else{
+        themeBtn.innerHTML="☀️";
+    }
+
+}
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+    const updateCounter = () => {
+
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+
+        const increment = target / 100;
+
+        if(count < target){
+
+            counter.innerText = Math.ceil(count + increment);
+
+            setTimeout(updateCounter,20);
+
+        }else{
+
+            counter.innerText = target;
+
+        }
+
+    };
+
+    updateCounter();
+
+});
+// Back To Top Button
+
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function(){
+
+    if(document.body.scrollTop > 300 || document.documentElement.scrollTop > 300){
+        topBtn.style.display = "block";
+    }else{
+        topBtn.style.display = "none";
+    }
+
+}
+
+topBtn.onclick = function(){
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+}
